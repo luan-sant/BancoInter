d1f9a6fbd32ad8376d9dc0aa78a0d876568bbb12
Luan Santos <luanlss10@outlook.com>
2022-02-11 16:15:37 -0300

    primeiro commit

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+        primary: string,
+        secondary: string,
+        tertiary: string,
+        background: string,
+        backgroundLight: string,
+        red: string,
+        green: string
+    };
+  }
+}
